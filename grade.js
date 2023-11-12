67d267ec8a628eecab223029f2d67f1fd8f594a0
MrSman-code <143835857+MrSman-code@users.noreply.github.com>
2023-11-12 13:45:17 +0100

    first commit

---
+++ grade.js
@@ -0,0 +1,33 @@
+mark = 90
+if(mark >= 70) {
+    alert('A');
+}
+else if(mark >=50 && mark <= 60 ){
+alert('B');
+}
+else if (mark >= 0 && mark <= 40 ) {
+    alert('F');
+}
+
+mark = 50
+if(mark >= 70) {
+    alert('A');
+}
+else if(mark >=50 && mark <= 60 ){
+alert('B');
+}
+else if (mark >= 0 && mark <= 40 ) {
+    alert('F');
+}
+
+mark = 39
+if(mark >= 70) {
+    alert('A');
+}
+else if(mark >=50 && mark <= 60 ){
+alert('B');
+}
+else if (mark >= 0 && mark <= 40 ) {
+    alert('F');
+}
+document.body.innerHTML = "Wheee i\'m messing with my html using Js"
